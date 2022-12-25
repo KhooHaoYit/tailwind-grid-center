d7e7a5ae6d0c16558f9e8075754047e421248104
Khoo Hao Yit <khoohaoyit16@gmail.com>
2022-12-25 06:19:01 +0000

    add all justify-content feature

--- src/index.js
+++ src/index.js
@@ -2,12 +2,34 @@
 
 const plugin = require('tailwindcss/plugin');
 
+const generateAllColumn = function* (targetCount) {
+  for (let itemCount = 1; itemCount < targetCount; ++itemCount) {
+    for (let index = 0; index < itemCount; ++index) {
+      yield [itemCount, index];
+    }
+  }
+}
+
+// https://stackoverflow.com/questions/147515/least-common-multiple-for-3-or-more-numbers
+const gcd = (a, b) => {
+  while (b) {
+    [a, b] = [b, a % b];
+  }
+  return a;
+}
+const lcm = (...args) => args.reduce((val, input) => val * input / gcd(val, input));
+
 module.exports = plugin.withOptions((options = {}) => {
   return ({ matchUtilities, theme }) => {
-    const values = theme('gridTemplateColumnsCenter');
-
     matchUtilities({
-      'grid-cols-center': (value) => {
+      'grid-cols-start': value => {
+        const targetCount = +value;
+        if (!Number.isInteger(targetCount)) return {};
+        return {
+          'grid-template-columns': `repeat(${value}, minmax(0, 1fr))`,
+        };
+      },
+      'grid-cols-center': value => {
         const targetCount = +value;
         if (!Number.isInteger(targetCount)) return {};
         const output = {
@@ -16,19 +38,125 @@ module.exports = plugin.withOptions((options = {}) => {
             'grid-column': 'span 2',
           },
         };
-        const centerOffset = -targetCount;
-        for (let fillableCount = 1; fillableCount < targetCount; ++fillableCount) {
-          for (let itemCount = 0; itemCount < fillableCount; ++itemCount) {
-            for (let index = 0; index <= itemCount; ++index) {
-              output[`> *:nth-last-child(${itemCount + 1 - index}):nth-child(${targetCount}n + ${index + 1})`] = {
-                'grid-column-end': `${centerOffset - itemCount + index * 2}`,
-              };
-            }
-          }
+        for (const [itemCount, index] of generateAllColumn(targetCount)) {
+          output[`> *:nth-last-child(${itemCount - index}):nth-child(${targetCount}n + ${index + 1})`] = {
+            'grid-column-end': `${-targetCount - itemCount - 1 + index * 2}`,
+          };
         }
         return output;
       },
-    }, { values });
+      'grid-cols-end': value => {
+        const targetCount = +value;
+        if (!Number.isInteger(targetCount)) return {};
+        const output = {
+          'grid-template-columns': `repeat(${value}, minmax(0, 1fr))`,
+        };
+        for (const [itemCount, index] of generateAllColumn(targetCount)) {
+          output[`> *:nth-last-child(${itemCount - index}):nth-child(${targetCount}n + ${index + 1})`] = {
+            'grid-column-end': `${targetCount + 1 - (itemCount - 1) + index}`,
+          };
+        }
+        return output;
+      },
+      /** Between visualization
+       * |-|-|-|-
+       * |- |- |-
+       * |-    |-
+       * 
+       * |||
+       * | |
+      */
+      /** */
+      'grid-cols-between': value => {
+        const targetCount = +value;
+        if (!Number.isInteger(targetCount)) return {};
+        const itemSize = lcm(...Array(targetCount - 1).fill().map((_, index) => index + 1)) / (targetCount - 1);
+        const output = {
+          'grid-template-columns': `repeat(${itemSize * targetCount}, minmax(0, 1fr))`,
+          '> *': {
+            'grid-column': `span ${itemSize}`,
+          },
+        };
+        for (const [itemCount, index] of generateAllColumn(targetCount)) {
+          if (itemCount === 1) continue;
+          const leftOffset = itemSize * (targetCount - 1) / (itemCount - 1);
+          output[`> *:nth-last-child(${itemCount - index}):nth-child(${targetCount}n + ${index + 1})`] = {
+            'grid-column-end': `${leftOffset * index + itemSize + 1}`,
+          };
+        }
+        return output;
+      },
+      /** Around visualization
+       * |---|---|---
+       *  |---  |---
+       *     |---
+       * 
+       * |-|-
+       *  |-
+      */
+      /** */
+      'grid-cols-around': value => {
+        const targetCount = +value;
+        if (!Number.isInteger(targetCount)) return {};
+        const multiplier = Array(targetCount - 1).fill().map((_, index) => {
+          const multiplier = [targetCount - 1 - index, (index + 1) * 2];
+          const divideable = gcd(...multiplier);
+          multiplier[0] /= divideable;
+          multiplier[1] /= divideable;
+          return multiplier;
+        });
+        const itemSize = lcm(...multiplier.map(val => val[1]));
+        const output = {
+          'grid-template-columns': `repeat(${itemSize * targetCount}, minmax(0, 1fr))`,
+          '> *': {
+            'grid-column': `span ${itemSize}`,
+          },
+        };
+        for (const [itemCount, index] of generateAllColumn(targetCount)) {
+          const currentMultiplier = multiplier.at(itemCount - 1);
+          const padSize = itemSize / currentMultiplier[1] * currentMultiplier[0];
+          output[`> *:nth-last-child(${itemCount - index}):nth-child(${targetCount}n + ${index + 1})`] = {
+            'grid-column-end': `${(padSize * 2 + itemSize) * index + padSize + itemSize + 1}`,
+          };
+        }
+        return output;
+      },
+      /** Evenly visualization
+       * |--|--|--
+       *  |-- |--
+       *    |--
+       * 
+       * |-|-
+       *  |-
+      */
+      /** */
+      'grid-cols-evenly': value => {
+        const targetCount = +value;
+        if (!Number.isInteger(targetCount)) return {};
+        const multiplier = Array(targetCount - 1).fill().map((_, index) => {
+          const multiplier = [targetCount - 1 - index, index + 2];
+          const divideable = gcd(...multiplier);
+          multiplier[0] /= divideable;
+          multiplier[1] /= divideable;
+          return multiplier;
+        });
+        const itemSize = lcm(...multiplier.map(val => val[1]));
+        const output = {
+          'grid-template-columns': `repeat(${itemSize * targetCount}, minmax(0, 1fr))`,
+          '> *': {
+            'grid-column': `span ${itemSize}`,
+          },
+        };
+        for (const [itemCount, index] of generateAllColumn(targetCount)) {
+          const currentMultiplier = multiplier.at(itemCount - 1);
+          const padSize = itemSize / currentMultiplier[1] * currentMultiplier[0];
+          output[`> *:nth-last-child(${itemCount - index}):nth-child(${targetCount}n + ${index + 1})`] = {
+            'grid-column-end': `${(padSize + itemSize) * (index + 1) + 1}`,
+          };
+        }
+        return output;
+      },
+    }, { values: theme('gridTemplateColumnsCenter') });
   };
 }, () => ({
   theme: {
